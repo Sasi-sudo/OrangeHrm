0799395f2d748952ff98a7dbc27306bd5cd240ca
Sasikeshwaran <98255438+Sasikeshwaran@users.noreply.github.com>
2025-11-17 10:28:35 +0530

    first commit

---
+++ OrangeHrm/src/test/java/org/Testing/AppTest.java
@@ -0,0 +1,18 @@
+package org.Testing;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AppTest {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	}
+
+}
